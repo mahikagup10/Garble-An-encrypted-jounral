abfe181cf8b2c1961639fbfa6e5f2716eececf6d
mahikagup10 <77979481+mahikagup10@users.noreply.github.com>
2022-11-13 20:10:06 +0530

    Update EncDecFiles.py

--- EncDecFiles.py
+++ EncDecFiles.py
@@ -1,14 +1,14 @@
 from Crypto.Cipher import AES
-from cryptography.fernet import Fernet
+from Crypto.Random import get_random_bytes
 
 
-def acceptKey():
-    key_string = input("Enter key of 16 characters:")
-    if len(key_string)!=16:
-        raise Exception("Key length not 16")
-    key = bytes(key_string, 'utf-8')
-    #print(key)
-    return key
+# def acceptKey():
+#     key_string = input("Enter key of 16 characters:")
+#     if len(key_string)!=16:
+#         raise Exception("Key length not 16")
+#     key = bytes(key_string, 'utf-8')
+#     #print(key)
+#     return key
 
 def encryptText(key,data):
     cipher = AES.new(key,AES.MODE_EAX)
@@ -24,13 +24,25 @@ def decryptText(key,ciphertext,nonce):
     plaintext = cipher.decrypt(ciphertext).decode()
     #print("Plaintext:",plaintext)
     return plaintext
+
+
+passw = input("Enter your key:")
+
+with open("passwd.txt", 'rb') as pw:
+    password = pw.read()
+
+if passw.encode()!= password:
+    print("incorrect key entered")
+    exit()
+
+key = get_random_bytes(16)
     
 #data = "hi this needs to be encrypted".encode()
 with open('test.txt', 'rb') as file:
     original = file.read()
 data = original
 #data = input("Enter diary entry:").encode()
-key = acceptKey()
+# key = acceptKey()
 ct,nonce = encryptText(key,data)
 
 with open('test.txt', 'wb') as encrypted_file:
