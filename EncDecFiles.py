719e00c1e3c6b4decc895465e2f0462a4169182a
mahikagup10 <77979481+mahikagup10@users.noreply.github.com>
2022-11-13 19:46:07 +0530

    Create EncDecFiles.py

---
+++ EncDecFiles.py
@@ -0,0 +1,57 @@
+from Crypto.Cipher import AES
+from cryptography.fernet import Fernet
+
+
+def acceptKey():
+    key_string = input("Enter key of 16 characters:")
+    if len(key_string)!=16:
+        raise Exception("Key length not 16")
+    key = bytes(key_string, 'utf-8')
+    #print(key)
+    return key
+
+def encryptText(key,data):
+    cipher = AES.new(key,AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext = cipher.encrypt(data)
+    #print("Ciphertext:",ciphertext)
+    return ciphertext,nonce
+
+def decryptText(key,ciphertext,nonce):
+    # key = acceptKey()
+    # ciphertext,nonce = encryptText()
+    cipher = AES.new(key,AES.MODE_EAX,nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext).decode()
+    #print("Plaintext:",plaintext)
+    return plaintext
+    
+#data = "hi this needs to be encrypted".encode()
+with open('test.txt', 'rb') as file:
+    original = file.read()
+data = original
+#data = input("Enter diary entry:").encode()
+key = acceptKey()
+ct,nonce = encryptText(key,data)
+
+with open('test.txt', 'wb') as encrypted_file:
+    encrypted_file.write(bytes(ct))
+
+print("Encrypted text:", ct)
+
+
+
+pt = decryptText(key,ct,nonce)
+print("Decrypted text:", pt)
+
+# with open('test.bin', 'rb') as enc_file:
+    # encrypted = enc_file.read()
+
+# decrypted = decryptText(key,ct,nonce)
+
+with open('test.txt', 'wb') as dec_file:
+    dec_file.write(pt.encode())
+
+
+
+
+
