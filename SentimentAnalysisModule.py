48bddf23be14648cac8c2e9c155d159e4d620ac7
Maani Jacob <54497498+MaaniacLaughter@users.noreply.github.com>
2022-11-16 10:25:01 +0530

    Added files to enable and test Sentiment analysis
    Added NLP enabled Sentiment Analysis using the nltk library in python. The module returns values from -1 to 1  indicating a spectrum of negative to positive sentiment.

---
+++ SentimentAnalysisModule.py
@@ -0,0 +1,18 @@
+from textblob import TextBlob
+from newspaper import Article
+import nltk
+
+def sentimentAnalysis():
+
+    #nltk.download('punkt')     #Use this line if running on a fresh machine
+
+    file = open("ex2.txt",'r')
+    #file = open("example.txt","r")
+
+    #print(file)
+    text = str([word.encode('utf-8') for word in file])
+    print(text)
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+
+    return(sentiment)   #varies from -1 to 1   negative to positive
